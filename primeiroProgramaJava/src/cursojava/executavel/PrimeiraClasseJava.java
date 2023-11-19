f626ed62a0374336d48fd4899fc5150eb2a49c94
Jose Helder <joseheldersb@gmail.com>
2023-11-19 14:36:17 -0300

    criacao do projeto

---
+++ primeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*comentário main método auto-executável*/
+	public static void main(String[] args) {
+		
+		/*sysout*/
+		System.out.println("meu primeiro programa em java heyss");
+		
+	}
+
+}
